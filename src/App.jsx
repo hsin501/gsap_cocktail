282589e2aa1ec2d3dcd97883160e2c66559df608
hsin <joanne45644@gmail.com>
2025-11-16 22:36:05 +0800

    fix menu to Menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
-import Menu from './components/menu';
+import Menu from './components/Menu';
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
